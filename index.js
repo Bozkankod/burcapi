84cc14aa45f837556d3b9916972b3fa3feb23c4a
Bozkankod <bozkankod@gmail.com>
2022-09-25 16:27:02 +0300

    test

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
-
-
+var fs = require("fs");
 
 app.get("/", (req, res) => {
     res.send("Hello");
 });
 
 
-app.get("/burclar",(req,res)=>{
-    res.send(importData);
+app.get('/burclar', function (req, res) {
+    fs.readFile(__dirname + "/" + "data.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
+    });
+})
+
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile(__dirname + "/" + "data.json", 'utf8', function (err, data) {
+        var users = JSON.parse(data);
+        var user = users["burc" + req.params.id]
+        res.end(JSON.stringify(user));
+    });
 })
 
 
-app.listen(process.env.PORT || 3000, function(){
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
